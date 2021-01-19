0476df2a4141d50f850d6f384b10b62f6acc9ed6
ttldtor <ttldtor@gmail.com>
2021-01-19 19:33:14 +0300

    LCS snake provider's skeleton

---
+++ source/diff/lcs_snake_provider.d
@@ -0,0 +1,25 @@
+/**
+    LCS (longest common subsequence) snake provider
+
+    Authors: ttldtor
+    Copyright: © 2019-2021 ttldtor
+    License: Subject to the terms of the BSL-1.0 license, as written in the included LICENSE file.
+ */
+
+module diff.lcs_snake_provider;
+
+import diff.v;
+import diff.snake;
+import diff.snake_pair;
+
+/**
+    Utility class that provides functions to calculate the longest common subsequence (LCS) for forward, backward and 
+    in-between estimations.
+ 
+    Params:
+        T = The type of the element the snakes will hold
+ */
+final class LcsSnakeProvider(T) {
+
+}
+
