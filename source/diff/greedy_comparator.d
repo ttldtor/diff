01cdf86b112485f3c35e69aaf3a9f0d6d3dc9f6d
ttldtor <ttldtor@gmail.com>
2021-02-09 22:40:36 +0300

    GreedyComparator.solveReverse

--- source/diff/greedy_comparator.d
+++ source/diff/greedy_comparator.d
@@ -40,16 +40,17 @@ final class GreedyComparator(T) {
             dest           = Elements of the second object. Usually the current object
             destSize       = The number of elements of the second object to compare
 
-        Returns: ok() if the ranges could be compared; Error string (err!void) otherwise
+        Returns: ok() if the solution was found; Error string (err!void) otherwise
 
      */
     Expected!void solveForward(R)(ref Snake!T[] snakes, const ref V[] vs, R source, int sourceSize, R dest, 
-        int destSize) 
+        int destSize)
     if (isRandomAccessRange!R || isSomeString!R)
+    in (vs.length > 0)
     {
         auto p = point(sourceSize, destSize);
 
-        for (int d = vs.length - 1; p.x > 0 || p.y > 0; d--) {
+        for (int d = vs.length.to!int - 1; p.x > 0 || p.y > 0; d--) {
             auto k = p.x - p.y;
             auto xEnd = vs[d][k];
             auto yEnd = xEnd - k;
@@ -68,9 +69,71 @@ final class GreedyComparator(T) {
                         d, k, p.x, p.y, xEnd, yEnd));
             }
 
+            //TODO: #1 fix the snakes' appending
+            /*
             if (snakes.length == 0 || !snakes[0].append(solution)) {
                 snakes = solution ~ snakes;
             }
+            */
+            snakes = solution ~ snakes;
+
+            p.x = solution.xStart;
+            p.y = solution.yStart;
+        }
+
+        return ok();
+    }
+
+    /**
+        Finds all snakes that lead to the solution by taking a snapshot of each end point after each iteration of d and
+        then working forward from 0 to d<sub>solution</sub>.
+
+        Params:
+            R              = The source & destination range type
+            snakes         = The possible solution paths for transforming object $(D_PARAM source) to $(D_PARAM dest)
+            vs             = All saved end points indexed on <em>d</em>
+            source         = Elements of the first object. Usually the original object
+            sourceSize     = The number of elements of the first object to compare
+            dest           = Elements of the second object. Usually the current object
+            destSize       = The number of elements of the second object to compare
+
+        Returns: ok() if the solution was found; Error string (err!void) otherwise
+
+     */
+    Expected!void solveReverse(R)(ref Snake!T[] snakes, const ref V[] vs, R source, int sourceSize, R dest, 
+        int destSize)
+    if (isRandomAccessRange!R || isSomeString!R)
+    in (vs.length > 0)
+    {
+        auto p = point(0, 0);
+
+        for (int d = vs.length.to!int - 1; p.x < sourceSize || p.y < destSize; d--) {
+            auto k = p.x - p.y;
+            auto xEnd = vs[d][k];
+            auto yEnd = xEnd - k;
+
+            if (xEnd != p.x || yEnd != p.y) {
+                return err(
+                    format!"GreedyComparator!T.solveReverse: No solution for d: %s, k: %s, p:(%s, %s), V:(%s, %s)"(d, k, 
+                        p.x, p.y, xEnd, yEnd));
+            }
+
+            auto solution = Snake!T.create(p.x, sourceSize - p.x, p.y, destSize - p.y, false, sourceSize - destSize, 
+                vs[d], k, d, source, dest);
+
+            if (solution.xEnd != p.x || solution.yEnd != p.y) {
+                return err(
+                    format!"GreedyComparator!T.solveReverse: Missed solution for d: %s, k: %s, p:(%s, %s), V:(%s, %s)"(
+                        d, k, p.x, p.y, xEnd, yEnd));
+            }
+
+            //TODO: #1 fix the snakes' appending
+            /*
+            if (snakes.length == 0 || !snakes[$ - 1].append(snake)) {
+                snakes ~= snake;
+            }
+            */
+            snakes ~= solution;
 
             p.x = solution.xStart;
             p.y = solution.yStart;
